85afd5377ed3e822a5d3ccd8acc6b84bebc31ef5
MonolithDragon <joo.lajos76@gmail.com>
2021-09-18 23:24:25 +0200

    Add index

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         items: 1
     });
 
-    // Top sale owl caruosel
+    // Top sale owl carousel
     $("#top-sale .owl-carousel").owlCarousel({
         loop: true,
         nav: true,
@@ -20,6 +20,51 @@ $(document).ready(function() {
             },
             1000: {
                 items: 5
+            }
+        }
+    });
+
+    // Isotope filter
+    let $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
+
+    // Filter items on button click
+    $(".button-group").on("click", "button", function() {
+        let filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+
+    // New phones owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+     // New phones owl carousel
+     $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
             }
         }
     });
